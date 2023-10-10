13ec6a7272c18eb130268c1217cd39cc4cebae56
JEETBHARDWAJ <bhardwajjeet408@gmail.com>
2023-10-10 22:30:19 +0530

    fvgbhnm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+
+import Navbar from "./com/Navbar";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
 import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
+
 import { Routes ,Route, Navigate} from 'react-router-dom';
+
+
+
+
+
 const App = () => {
   return (
     <>
+      <Navbar/>
       <Routes>
       <Route exact path='/' element={<Home/>} />
       <Route exact path='/About' element={<About/>} />
